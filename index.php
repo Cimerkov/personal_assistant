6bcb227da6df21b0fe7bccc04bde24c04ba343a1
Cimerkov <90925639+Cimerkov@users.noreply.github.com>
2023-11-03 13:40:00 +0100

    Contact

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./assets/style.css">
+
 </head>
 
 <body>
@@ -41,7 +42,7 @@
                         <a class="inline-block text-gray-300 no-underline hover:text-white hover:text-underline py-2 px-4" href="#services" @click="isOpen = false">Services</a>
                     </li>
                     <li class="mr-3">
-                        <a class="inline-block text-gray-300 no-underline hover:text-white hover:text-underline py-2 px-4" href="#" @click="isOpen = false">Contact</a>
+                        <a class="inline-block text-gray-300 no-underline hover:text-white hover:text-underline py-2 px-4" href="#contact" @click="isOpen = false">Contact</a>
                     </li>
                 </ul>
             </div>
@@ -172,6 +173,110 @@
 
     </section>
 
+    <!-- Contact -->
+
+    <section id="#contact">
+        <div class="text-gray-500 font-bold text-3xl text-center p-5">
+            <h1>Contact</h1>
+        </div>
+
+        <div class="containerxl mx-auto flex flex-col md:flex-row p-5 bg-gray-800">
+            <div class="w-full md:w-1/2">
+                <div class="max-w-md mx-auto">
+                    <form class="bg-white shadow-md rounded px-6 pt-4 pb-8 mb-2">
+                        <div class="mb-4">
+                            <div class="flex flex-wrap -mx-3">
+                                <div class="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+                                    <label class="block text-gray-700 text-sm font-bold mb-2" for="firstName">First Name</label>
+                                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="firstName" type="text" placeholder="First Name" required>
+                                </div>
+                                <div class="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+                                    <label class="block text-gray-700 text-sm font-bold mb-2" for="LastName">Last Name</label>
+                                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="lastName" type="text" placeholder="Last Name" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <div class="flex flex-wrap -mx-3">
+                                <div class="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+                                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
+                                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" required>
+                                </div>
+                                <div class="w-full md:w-1/2 px-3 mb-4 md:mb-0">
+                                    <label class="block text-gray-700 text-sm font-bold mb-2" for="phone">Phone</label>
+                                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="phone" type="text" placeholder="Phone" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-6">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="message">Message</label>
+                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" rows="4" placeholder="Message" required></textarea>
+                        </div>
+                        <div class="text-center">
+                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Send</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="w-full md:w-1/2 text-center mt-4 md:mt-0">
+                <div class="mx-auto p-4">
+                    <div class="space-y-4">
+                        <div>
+                            <h3 class="text-white text-xl font-bold">Address</h3>
+                            <p class="text-white">Str. 1520 no:8, Skopje, Macedonia</p>
+                        </div>
+                        <div>
+                            <h3 class="text-white text-xl font-bold">Email</h3>
+                            <p class="text-white">personal_assistant@yahoo.com</p>
+                        </div>
+                        <div>
+                            <h3 class="text-white text-xl font-bold">Phone</h3>
+                            <p class="text-white">+389701111111</p>
+                        </div>
+                        <div class="pt-12">
+                            <h3 class="text-white text-2xl font-bold uppercase">Follow Us</h3>
+                        </div>
+                        <div class="">
+                            <ul class="flex justify-center space-x-4 md:space-x-8">
+                                <li class="p-2">
+                                    <a class="" aria-label="Visit TrendyMinds LinkedIn" href="https://www.linkedin.com/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-8">
+                                            <path fill="white" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z">
+                                            </path>
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li class="p-2">
+                                    <a class="" aria-label="Visit TrendyMinds YouTube" href="https://www.youtube.com/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="h-8">
+                                            <path fill="white" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z">
+                                            </path>
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li class="p-2">
+                                    <a class="" aria-label="Visit TrendyMinds Facebook" href="https://www.facebook.com/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="h-8">
+                                            <path fill="white" d="m279.14 288 14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
+                                            </path>
+                                        </svg>
+                                    </a>
+                                </li>
+                                <li class="p-2">
+                                    <a class="" aria-label="Visit TrendyMinds Instagram" href="https://www.instagram.com/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-8">
+                                            <path fill="white" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
+                                            </path>
+                                        </svg>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="bg-gray-800">
+        <h1 class="text-center text-white font-bold p-4">© 2023 Copyright: Cimerkov </h1>
+    </footer>
 
 
 
